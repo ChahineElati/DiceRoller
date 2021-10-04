e105f34a7735ce4c872fd5d2e9a37432cdaaac6f
ChahineElati <chahinosaiyan@gmail.com>
2021-10-04 11:43:49 +0100

    roll function with pictures

--- app/src/main/java/com/chahine_elati/diceroller/MainActivity.kt
+++ app/src/main/java/com/chahine_elati/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.chahine_elati.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -26,8 +27,16 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        //val resultTextView: TextView = findViewById(R.id.textView)
-        //resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        when(diceRoll) {
+            1-> diceImage.setImageResource(R.drawable.dice_1)
+            2-> diceImage.setImageResource(R.drawable.dice_2)
+            3-> diceImage.setImageResource(R.drawable.dice_3)
+            4-> diceImage.setImageResource(R.drawable.dice_4)
+            5-> diceImage.setImageResource(R.drawable.dice_5)
+            else-> diceImage.setImageResource(R.drawable.dice_6)
+
+        }
     }
 
 }
